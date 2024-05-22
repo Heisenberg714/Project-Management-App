efe691b5dd7af2a2e575dbaf76eccdb36a568958
Leon <leontvshow714@gmail.com>
2024-05-23 00:52:20 +0930

    collecting user input with Refs and forwarded refs

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,41 @@ import ProjectsSidebar from "./components/ProjectsSidebar";
 function App() {
   const [projectState, setProjectState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
   });
 
   function handleStartAddProject() {
-    setProjectState(prevState => {
+    setProjectState((prevState) => {
       return {
         ...prevState,
         selectedProjectId: null,
       };
-    })
+    });
   }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectState);
 
   let content;
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
-
 
   return (
     <main className="h-screen my-8 flex gap-8">
